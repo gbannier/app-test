a1dd9cf49e760b5d8621f9200e3e2fe6df0ab1a0
gordonbannier <aaaa>
2019-05-05 17:18:30 +0200

    add cancel und save methods

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule }    from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from '../in-memory-data.service';
-import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AppUIRoutingModule } from './app-ui-routing.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from '../in-memory-data.service';
+import {LayoutComponent} from './layout/layout.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {AppUIRoutingModule} from './app-ui-routing.module';
 import {LocationsComponent} from "../locations/locations.component";
 import {LocationDetailComponent} from "../location-detail/location-detail.component";
+
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent, LocationsComponent, LocationDetailComponent],
   imports: [CommonModule, AppUIRoutingModule, FormsModule, HttpClientModule,HttpClientInMemoryWebApiModule.forRoot(
-    InMemoryDataService, { dataEncapsulation: false }
+    InMemoryDataService, {dataEncapsulation: false},
   )],
   exports:[LayoutComponent]
 })
